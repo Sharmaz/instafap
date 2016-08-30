731b771c7f6402e54294f96eb33a098f30f4b162
Sharmaz <irae45@gmail.com>
2016-08-29 22:23:08 -0500

    Agregada funcionalidad de like con doble click en una imagen del timeline

--- client/picture-card/index.js
+++ client/picture-card/index.js
@@ -9,11 +9,12 @@ module.exports = function pictureCard(pic) {
   function render(picture) {
       return yo`<div class="card ${picture.liked ? 'liked' : ''}">
     <div class="card-image">
-      <img class="activator" src="${picture.url}">
+      <img class="activator" src="${picture.url}" ondblclick=${like.bind(null, null, true)} />
+      <i class="fa fa-heart like-heart ${picture.likeHeart ? 'liked' : ''}"></i>
     </div>
     <div class="card-content">
       <a href="/${picture.user.username}"class="card-title">
-        <img src="${picture.user.avatar}" class="avatar"/>
+        <img src="${picture.user.avatar}" class="avatar" />
         <span class="username">${picture.user.username}</span>
       </a>
       <small class="right time">${translate.date.format(picture.createdAt)}</small>
@@ -26,11 +27,29 @@ module.exports = function pictureCard(pic) {
   </div>`
   }
 
-  function like(liked) {
+  function like(liked, dblclick) {
+    if (dblclick) {
+      pic.likeHeart = pic.liked = !pic.liked
+      liked = pic.liked
+    } else {
+      pic.liked = liked
+    }
+
     pic.liked = liked
     pic.likes += liked ? 1 : -1
-    var newEl = render(pic)
-    yo.update(el, newEl)
+    
+    function doRender() {
+      var newEl = render(pic)
+      yo.update(el, newEl)
+    }
+
+    doRender()
+
+    setTimeout(function () {
+      pic.likeHeart = false
+      doRender()
+    }, 1500)
+
     return false
   }
 
